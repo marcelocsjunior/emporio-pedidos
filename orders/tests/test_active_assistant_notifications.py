1d3f1e787af2c3a72e53fd57d42407bb104b1bb7
Marcelo Jr - MCSJ <111250648+marcelocsjunior@users.noreply.github.com>
2026-07-18 22:35:35 -0300

    style: separar import local no teste de notificações

--- orders/tests/test_active_assistant_notifications.py
+++ orders/tests/test_active_assistant_notifications.py
@@ -8,9 +8,10 @@ from django.contrib.auth.models import Group
 from django.test import TestCase, override_settings
 from django.urls import reverse
 from django.utils import timezone
-from intelligence.active_assistant import notify_order_created
 from intelligence.models import AIRecommendation
 from orders.models import AuditEvent, Company, Order, OrderItem, Product
+
+from intelligence.active_assistant import notify_order_created
 
 
 @override_settings(
